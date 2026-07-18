637b4c1f25e91b7e3fc7de387ad2bb8b087b7242
rsantosfps-cmd <rsantosfps@gmail.com>
2026-07-18 20:19:37 -0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,36 +1,66 @@
 let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
 
-
-function salvarProduto(){
-
-let produto = {
-
-codigo: document.getElementById("codigo").value,
-
-nome: document.getElementById("produto").value,
-
-preco: document.getElementById("preco").value,
-
-estoque: document.getElementById("estoque").value
-
-};
+let editando = null;
 
 
-produtos.push(produto);
+// Gerar código de barras automático
+function gerarCodigo(){
 
+    let codigo = "";
 
-localStorage.setItem(
-"produtos",
-JSON.stringify(produtos)
-);
+    for(let i = 0; i < 12; i++){
+        codigo += Math.floor(Math.random() * 10);
+    }
 
-
-mostrarProdutos();
-
+    return codigo;
 
 }
 
 
+// salvar ou atualizar produto
+function salvarProduto(){
+
+    let produto = {
+
+        codigo: document.getElementById("codigo").value || gerarCodigo(),
+
+        nome: document.getElementById("produto").value,
+
+        preco: document.getElementById("preco").value,
+
+        estoque: document.getElementById("estoque").value
+
+    };
+
+
+    if(editando !== null){
+
+        produtos[editando] = produto;
+
+        editando = null;
+
+    }else{
+
+        produtos.push(produto);
+
+    }
+
+
+    localStorage.setItem(
+        "produtos",
+        JSON.stringify(produtos)
+    );
+
+
+    limparCampos();
+
+    mostrarProdutos();
+
+}
+
+
+
+// mostrar produtos
 function mostrarProdutos(){
 
 let lista = document.getElementById("lista");
@@ -38,7 +68,7 @@ let lista = document.getElementById("lista");
 lista.innerHTML="";
 
 
-produtos.forEach(p=>{
+produtos.forEach((p,index)=>{
 
 
 lista.innerHTML += `
@@ -53,12 +83,89 @@ lista.innerHTML += `
 
 <td>${p.estoque}</td>
 
+
+<td>
+
+<button onclick="editarProduto(${index})">
+Editar
+</button>
+
+<button onclick="excluirProduto(${index})">
+Excluir
+</button>
+
+</td>
+
+
 </tr>
 
 `;
 
 });
 
+
+}
+
+
+
+// carregar produto para edição
+
+function editarProduto(index){
+
+let p = produtos[index];
+
+
+document.getElementById("codigo").value = p.codigo;
+
+document.getElementById("produto").value = p.nome;
+
+document.getElementById("preco").value = p.preco;
+
+document.getElementById("estoque").value = p.estoque;
+
+
+editando = index;
+
+
+}
+
+
+// excluir
+
+function excluirProduto(index){
+
+if(confirm("Excluir produto?")){
+
+
+produtos.splice(index,1);
+
+
+localStorage.setItem(
+"produtos",
+JSON.stringify(produtos)
+);
+
+
+mostrarProdutos();
+
+
+}
+
+}
+
+
+
+// limpar
+
+function limparCampos(){
+
+document.getElementById("codigo").value="";
+
+document.getElementById("produto").value="";
+
+document.getElementById("preco").value="";
+
+document.getElementById("estoque").value="";
 
 }
 
